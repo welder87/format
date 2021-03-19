ef0e248a60b352bd3c6898f501e86150c97d82f4
welder87 <25900160+welder87@users.noreply.github.com>
2021-03-19 23:55:13 +0500

    add base urls

---
+++ format/urls.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+
+import debug_toolbar
+
+from common.routers import router
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    ) + static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
